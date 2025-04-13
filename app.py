c6871690121219632ff1487c147d8b3c47bf1e91
twoweekinnovations <twoweekinnovations@gmail.com>
2025-04-13 15:48:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ logging.basicConfig(level=logging.DEBUG)
 # Email settings
 SMTP_SERVER = os.environ.get('SMTP_SERVER', 'smtp.gmail.com')  # SMTP Server (e.g., Gmail)
 SMTP_PORT = int(os.environ.get('SMTP_PORT', 587))  # SMTP Port (587 for Gmail)
-SENDER_EMAIL = os.environ.get('EMAIL_FROM', '')  # Sender's email address
-SENDER_PASSWORD = os.environ.get('EMAIL_PASS', 'iznf hcqu wplo cblw')  # Sender's email password or app-specific password
-RECEIVER_EMAIL = os.environ.get('EMAIL_TO', 'twoweekinnovations@gmail.com')  # Receiver's email address
+SENDER_EMAIL = os.environ.get('SENDER_EMAIL', 'twoweekinnovations@gmail.com')  # Sender's email address
+SENDER_PASSWORD = os.environ.get('SENDER_PASSWORD', 'iznf hcqu wplo cblw')  # Sender's email password or app-specific password
+RECEIVER_EMAIL = os.environ.get('RECEIVER_EMAIL', 'twoweekinnovations@gmail.com')  # Receiver's email address
 
 # Product catalog organized by categories (in a real app, this would come from a database)
 PRODUCTS = [
